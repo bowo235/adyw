1b332edb36a06912602c597bf1f61a064248a2a0
bowo235 <bowoady535@gmail.com>
2026-04-13 12:07:38 +0800

    commit pertamaku

---
+++ index1.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "adyw";
+$umur = 20;
+$tinggi = 165;
+$menikah = true;
+$hobi = ["membaca", "olahraga", "maingamr"];
+
+echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi cm, statu saya $menikah, hobi saya $hobi[0] , $hobi[1] , $hobi[2]";
+?>
